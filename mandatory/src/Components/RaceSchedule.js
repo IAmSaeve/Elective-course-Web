7739508712bd0d25efc8327d52bc2e5873831179
Sebastian Petersen <sebkirller@gmail.com>
2019-04-15 22:42:15 +0200

    Fucking end me!

--- mandatory/src/Components/RaceSchedule.js
+++ mandatory/src/Components/RaceSchedule.js
@@ -6,28 +6,39 @@ import { DateRange } from 'moment-range';
 import Dayz from 'dayz';
 import '../../node_modules/dayz/dist/dayz.css';
 
-const EVENTS = new Dayz.EventsCollection([
-    {
-        content: 'A short event',
-        range: new DateRange(moment.now(),
-            moment.now())
-    }
-]);
 
-const relativeDate = moment()
-var t = '05:10:00Z'
-var s = new moment('2019-03-17 ' + t.slice(0, -1))
+
+// var t = ''
+// var s = new moment('2019-03-17 ' + t.slice(0, -1))
+
 
 class RaceSchedule extends Component {
 
     componentDidMount() {
-        console.log(this.props)
+        // console.log(this.props)
         this.props.fetchRaceSchedule()
     }
 
     render() {
-        // console.log(s) // Debugging
-        console.log(this.props)
+
+        const relativeDate = moment()
+        const EVENTS = new Dayz.EventsCollection([
+            {
+                content: 'A short event',
+                range: new DateRange(moment.now(),
+                    moment.now())
+            }
+        ]);
+
+        // FIXME: This shit not working fucking undefined my ass cock sucking mother fucker
+        // I am going to bed and you better be working tomorrow you fuck piece of shit!
+        if (typeof this.props.races === 'undefined') {
+            var s = new moment('2019-03-17 ' + this.props.races[0].time.slice(0, -1))
+            console.log(s)
+        }
+
+        // console.log(this.props.races) // Debugging
+
         return (
             <div className='row' style={{ marginTop: '15rem' }}>
                 <div className='col' style={{ borderWidth: '3px', borderColor: 'White', borderStyle: 'solid' }}>
@@ -48,4 +59,4 @@ const mapStateToProps = state => {
     return { races: state.races }
 }
 
-export default connect(mapStateToProps, { fetchRaceSchedule})(RaceSchedule)
+export default connect(mapStateToProps, { fetchRaceSchedule })(RaceSchedule)
