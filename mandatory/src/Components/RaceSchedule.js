4cdaa4de9c7868ad08cd25f2761487c3fc487e85
Sebastian Petersen <sebkirller@gmail.com>
2019-04-15 21:57:09 +0200

    Semi working calendar

--- mandatory/src/Components/RaceSchedule.js
+++ mandatory/src/Components/RaceSchedule.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import moment from 'moment';
+import { DateRange } from 'moment-range';
+import Dayz from 'dayz';
+import '../../node_modules/dayz/dist/dayz.css';
+
+const EVENTS = new Dayz.EventsCollection([
+    {
+        content: 'A short event',
+        range: new DateRange(moment.now(),
+            moment.now())
+    }
+]);
+
+const relativeDate = moment()
+
+var s = new moment('2019-03-17 05:10:00')
 
 class RaceSchedule extends Component {
     render() {
+console.log(s)
         return (
-            <div>
-                <h1>TEST</h1>
+            <div className='row' style={{ marginTop: '15rem' }}>
+                <div className='col' style={{ borderWidth: '3px', borderColor: 'White', borderStyle: 'solid' }}>
+                    <div className='p-3'>
+                        <Dayz
+                            display='month'
+                            date={relativeDate}
+                            events={EVENTS}
+                        />
+                    </div>
+                </div>
             </div>
         )
     }
