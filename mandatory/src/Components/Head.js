fa388bd782e654227aec4c1aa3e54d94a979b979
Sebastian Petersen <sebkirller@gmail.com>
2019-04-16 22:16:58 +0200

    User friendly naming for navbar buttons

--- mandatory/src/Components/Head.js
+++ mandatory/src/Components/Head.js
@@ -8,9 +8,9 @@ class Header extends Component {
                 <nav className='navbar navbar-expand-lg navbar-dark bg-primary pl-0'>
                     <div className='collapse navbar-collapse' id='navbarColor01'>
                         <Link to='/' className='nav-item nav-link btn btn-outline-success ml-1'>Home</Link>
-                        <Link to='/DriverList' className='nav-item nav-link btn btn-outline-success ml-3'>DriverList</Link>
-                        <Link to='/RaceResults' className='nav-item nav-link btn btn-outline-success ml-3'>RaceResults</Link>
-                        <Link to='/RaceSchedule' className='nav-item nav-link btn btn-outline-success ml-3'>RaceSchedule</Link>
+                        <Link to='/DriverList' className='nav-item nav-link btn btn-outline-success ml-3'>Drivers</Link>
+                        <Link to='/RaceResults' className='nav-item nav-link btn btn-outline-success ml-3'>Results</Link>
+                        <Link to='/RaceSchedule' className='nav-item nav-link btn btn-outline-success ml-3'>Schedule</Link>
                     </div>
                 </nav>
             </div>
